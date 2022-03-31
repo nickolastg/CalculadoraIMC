6f8ee52d2b173a30c99f48705b63cf85d23fca3c
Nickolas Tavares Garcia <nickmim969@gmail.com>
2022-03-30 21:14:15 -0300

    Solicita dados do usuario, calcula e apresenta imc

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Entre com sua altura em metros. Ex: 1,70");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Entre com sua altura em metros. Ex: 69,4");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine("{0}, sua altura é {1} e seu peso {2}", nome, altura, peso);
+
+            Console.WriteLine("Seu IMC é {0}", imc);
+        }
+    }
+}
